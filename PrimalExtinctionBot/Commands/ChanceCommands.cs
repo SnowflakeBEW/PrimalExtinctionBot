8ebcdd0b830709548801a9e71d56b7de936220ac
SnowflakeBEW <59858672+SnowflakeBEW@users.noreply.github.com>
2020-02-05 23:06:28 -0700

    Update ChanceCommands.cs

--- PrimalExtinctionBot/Commands/ChanceCommands.cs
+++ PrimalExtinctionBot/Commands/ChanceCommands.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace PrimalExtinctionBot.Commands
 {
-    public class ChanceCommands : BaseCommandModule
+     public class ChanceCommands : BaseCommandModule
     {
 
         [Command("Flip")]
@@ -36,165 +36,143 @@ namespace PrimalExtinctionBot.Commands
         }
 
 
-        [Command("Dice")]
-        [Description("Gives you the command to roll for the following sided dice: 'd4', 'd6', 'd8', 'd10', 'd12', and 'd20' ")]
-        public async Task Dice(CommandContext ctx)
+        [Command("Roll")]
+        [Description("Rolls a Dice")]
+        [RequireCategories(ChannelCheckMode.Any, "Bots")]
+        public async Task Roll(CommandContext ctx)
         {
-
-            var d4Step = new TextStep("Please use the commands '?d4' to roll a four-sided dice.", nextStep: null);
-            var d6Step = new TextStep("Please use the commands '?d6' to roll a six-sided dice.", nextStep: null);
-            var d8Step = new TextStep("Please use the commands '?d8' to roll an eight-sided dice.", nextStep: null);
-            var d10Step = new TextStep("Please use the commands '?d10' to roll a ten-sided dice.", nextStep: null);
-            var d12Step = new TextStep("Please use the commands '?d12' to roll a twelve-sided dice.", nextStep: null);
-            var d20Step = new TextStep("Please use the commands '?d20' to roll a twenty-sided dice.", nextStep: null);
-
-
-            var DiceStep = new ReactionStep("What sided dice do you want to roll? (Note: :one: = 12-Sided and :two: = 20-Sided)", new Dictionary<DiscordEmoji, ReactionStepData>
+            var rollEmbed = new DiscordEmbedBuilder
             {
-                { DiscordEmoji.FromName(ctx.Client, ":four:"), new ReactionStepData { Content = "A 4-Sided Dice?", NextStep = d4Step } },
-                { DiscordEmoji.FromName(ctx.Client, ":six:"), new ReactionStepData { Content = "A 6-Sided Dice?", NextStep = d6Step } },
-                { DiscordEmoji.FromName(ctx.Client, ":eight:"), new ReactionStepData { Content = "An 8-Sided Dice?", NextStep = d8Step } },
-                { DiscordEmoji.FromName(ctx.Client, ":keycap_ten:"), new ReactionStepData { Content = "A 10-Sided Dice?", NextStep = d10Step } },
-                {DiscordEmoji.FromName(ctx.Client, ":one:"), new ReactionStepData {Content = "A 12-Sided Dice?", NextStep = d12Step } },
-                { DiscordEmoji.FromName(ctx.Client, ":two:"), new ReactionStepData { Content = "A 20-Sided Dice?", NextStep = d20Step } }
-            });
-
-            var inputDialogueHandler = new DialogueHandler(
-                ctx.Client,
-                ctx.Channel,
-                ctx.User,
-                DiceStep
-                );
-
-            bool succeeded = await inputDialogueHandler.ProcessDialogue().ConfigureAwait(false);
-
-            if (!succeeded) { return; }
-        }
-
-        [Command("d4")]
-        [Description("Rolls a four-sided dice")]
-
-        public async Task d4(CommandContext ctx)
-        {
-
-            var Dice4Result = new Random();
-            string[] Answer = { "One!", "Two!", "Three!", "Four!" };
-
-
-            int bIndex = Dice4Result.Next(Answer.Length);
-
-            var d4embed = new DiscordEmbedBuilder
-            {
-                Title = "Roll a four-sided dice!",
-                Description = Answer[bIndex]
+                Title = "What sided dice would you like to roll?",
+                Description = ":four: (4) , :six: (6) , :eight: (8) , :keycap_ten: (10) , :one: (12) , or :two: (20) ?",
+                Color = DiscordColor.Green
             };
 
-            var RollFourMessage = await ctx.Channel.SendMessageAsync(embed: d4embed).ConfigureAwait(false);
-        }
+            var rollMessage = await ctx.Channel.SendMessageAsync(embed: rollEmbed).ConfigureAwait(false);
 
-        [Command("d6")]
-        [Description("Rolls a six-sided dice")]
+            var fourEmoji = DiscordEmoji.FromName(ctx.Client, ":four:");
+            var sixEmoji = DiscordEmoji.FromName(ctx.Client, ":six:");
+            var eightEmoji = DiscordEmoji.FromName(ctx.Client, ":eight:");
+            var tenEmoji = DiscordEmoji.FromName(ctx.Client, ":keycap_ten:");
+            var twelveEmoji = DiscordEmoji.FromName(ctx.Client, ":one:");
+            var twentyEmoji = DiscordEmoji.FromName(ctx.Client, ":two:");
 
-        public async Task d6(CommandContext ctx)
-        {
+            await rollMessage.CreateReactionAsync(fourEmoji).ConfigureAwait(false);
+            await rollMessage.CreateReactionAsync(sixEmoji).ConfigureAwait(false);
+            await rollMessage.CreateReactionAsync(eightEmoji).ConfigureAwait(false);
+            await rollMessage.CreateReactionAsync(tenEmoji).ConfigureAwait(false);
+            await rollMessage.CreateReactionAsync(twelveEmoji).ConfigureAwait(false);
+            await rollMessage.CreateReactionAsync(twentyEmoji).ConfigureAwait(false);
 
-            var Dice6Result = new Random();
-            string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!" };
+            var interactivity = ctx.Client.GetInteractivity();
 
+            var reactionResult = await interactivity.WaitForReactionAsync(
+                x => x.Message == rollMessage &&
+                x.User == ctx.User &&
+               (x.Emoji == fourEmoji || x.Emoji == sixEmoji || x.Emoji == eightEmoji || x.Emoji == tenEmoji || x.Emoji == twelveEmoji || x.Emoji == twentyEmoji)).ConfigureAwait(false);
 
-            int cIndex = Dice6Result.Next(Answer.Length);
-
-            var d6embed = new DiscordEmbedBuilder
+            if (reactionResult.Result.Emoji == fourEmoji)
             {
-                Title = "Roll a six-sided dice!",
-                Description = Answer[cIndex]
-            };
-
-            var RollSixMessage = await ctx.Channel.SendMessageAsync(embed: d6embed).ConfigureAwait(false);
-        }
-
-        [Command("d8")]
-        [Description("Rolls an eight-sided dice")]
-
-        public async Task d8(CommandContext ctx)
-        {
-
-            var Dice8Result = new Random();
-            string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!" };
+                var Dice4Result = new Random();
+                string[] Answer = { "One!", "Two!", "Three!", "Four!" };
 
 
-            int dIndex = Dice8Result.Next(Answer.Length);
+                int bIndex = Dice4Result.Next(Answer.Length);
 
-            var d8embed = new DiscordEmbedBuilder
+                var d4embed = new DiscordEmbedBuilder
+                {
+                    Title = "Roll a four-sided dice!",
+                    Description = Answer[bIndex]
+                };
+
+                var RollFourMessage = await ctx.Channel.SendMessageAsync(embed: d4embed).ConfigureAwait(false);
+            }
+            else if (reactionResult.Result.Emoji == sixEmoji)
             {
-                Title = "Roll a four-sided dice!",
-                Description = Answer[dIndex]
-            };
-
-            var RollEightMessage = await ctx.Channel.SendMessageAsync(embed: d8embed).ConfigureAwait(false);
-        }
-
-        [Command("d10")]
-        [Description("Rolls a ten-sided dice")]
-
-        public async Task d10(CommandContext ctx)
-        {
-
-            var Dice10Result = new Random();
-            string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!", "Nine!", "Ten!" };
+                var Dice6Result = new Random();
+                string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!" };
 
 
-            int eIndex = Dice10Result.Next(Answer.Length);
+                int cIndex = Dice6Result.Next(Answer.Length);
 
-            var d10embed = new DiscordEmbedBuilder
+                var d6embed = new DiscordEmbedBuilder
+                {
+                    Title = "Roll a six-sided dice!",
+                    Description = Answer[cIndex]
+                };
+
+                var RollSixMessage = await ctx.Channel.SendMessageAsync(embed: d6embed).ConfigureAwait(false);
+            }
+            else if (reactionResult.Result.Emoji == eightEmoji)
             {
-                Title = "Roll a ten-sided dice!",
-                Description = Answer[eIndex]
-            };
-
-            var RollTenMessage = await ctx.Channel.SendMessageAsync(embed: d10embed).ConfigureAwait(false);
-        }
-
-        [Command("d12")]
-        [Description("Rolls a twelve-sided dice")]
-
-        public async Task d12(CommandContext ctx)
-        {
-
-            var Dice12Result = new Random();
-            string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!", "Nine!", "Ten!", "Eleven!", "Twelve!" };
+                var Dice8Result = new Random();
+                string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!" };
 
 
-            int fIndex = Dice12Result.Next(Answer.Length);
+                int dIndex = Dice8Result.Next(Answer.Length);
 
-            var d12embed = new DiscordEmbedBuilder
+                var d8embed = new DiscordEmbedBuilder
+                {
+                    Title = "Roll an eight-sided dice!",
+                    Description = Answer[dIndex]
+                };
+
+                var RollEightMessage = await ctx.Channel.SendMessageAsync(embed: d8embed).ConfigureAwait(false);
+            }
+            else if (reactionResult.Result.Emoji == tenEmoji)
             {
-                Title = "Roll a ten-sided dice!",
-                Description = Answer[fIndex]
-            };
-
-            var RollTwelveMessage = await ctx.Channel.SendMessageAsync(embed: d12embed).ConfigureAwait(false);
-        }
-
-        [Command("d20")]
-        [Description("Rolls a twenty-sided dice")]
-
-        public async Task d20(CommandContext ctx)
-        {
-
-            var Dice20Result = new Random();
-            string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!", "Nine!", "Ten!", "Eleven!", "Twelve!", "Thirteen!", "Fourteen!", "Fifteen!", "Sixteen!", "Seventeen!", "Eighteen!", "Nineteen!", "Twenty!" };
+                var Dice10Result = new Random();
+                string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!", "Nine!", "Ten!" };
 
 
-            int gIndex = Dice20Result.Next(Answer.Length);
+                int eIndex = Dice10Result.Next(Answer.Length);
 
-            var d20embed = new DiscordEmbedBuilder
+                var d10embed = new DiscordEmbedBuilder
+                {
+                    Title = "Roll a ten-sided dice!",
+                    Description = Answer[eIndex]
+                };
+
+                var RollTenMessage = await ctx.Channel.SendMessageAsync(embed: d10embed).ConfigureAwait(false);
+            }
+            else if (reactionResult.Result.Emoji == twelveEmoji)
             {
-                Title = "Roll a twenty-sided dice!",
-                Description = Answer[gIndex]
-            };
+                var Dice12Result = new Random();
+                string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!", "Nine!", "Ten!", "Eleven!", "Twelve!" };
 
-            var RollTwentyMessage = await ctx.Channel.SendMessageAsync(embed: d20embed).ConfigureAwait(false);
+
+                int fIndex = Dice12Result.Next(Answer.Length);
+
+                var d12embed = new DiscordEmbedBuilder
+                {
+                    Title = "Roll a ten-sided dice!",
+                    Description = Answer[fIndex]
+                };
+
+                var RollTwelveMessage = await ctx.Channel.SendMessageAsync(embed: d12embed).ConfigureAwait(false);
+            }
+            else if (reactionResult.Result.Emoji == twentyEmoji)
+            {
+                var Dice20Result = new Random();
+                string[] Answer = { "One!", "Two!", "Three!", "Four!", "Five!", "Six!", "Seven!", "Eight!", "Nine!", "Ten!", "Eleven!", "Twelve!", "Thirteen!", "Fourteen!", "Fifteen!", "Sixteen!", "Seventeen!", "Eighteen!", "Nineteen!", "Twenty!" };
+
+
+                int gIndex = Dice20Result.Next(Answer.Length);
+
+                var d20embed = new DiscordEmbedBuilder
+                {
+                    Title = "Roll a twenty-sided dice!",
+                    Description = Answer[gIndex]
+                };
+
+                var RollTwentyMessage = await ctx.Channel.SendMessageAsync(embed: d20embed).ConfigureAwait(false);
+            }
+            else
+            {
+                // Do Nothing
+            }
+
+            await rollMessage.DeleteAsync().ConfigureAwait(false);
         }
     }
 }
